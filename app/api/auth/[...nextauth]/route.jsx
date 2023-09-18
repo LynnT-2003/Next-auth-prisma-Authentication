44c1cb45fd0b171d243b781100f4d8f21bca3301
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-09-19 03:35:53 +0700

    Setup NextAuth catch all route.

---
+++ app/api/auth/[...nextauth]/route.jsx
@@ -0,0 +1,47 @@
+import nextAuth from "next-auth";
+import prisma from "../../../libs/prismadb";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "John Smith",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: 1, name: "John Smith", email: "jsmith@example.com" };
+        return user;
+      },
+    }),
+  ],
+  secret: process.env.SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  debug: process.env.NODE_ENV === "development",
+};
+
+// export as handlers for /api/auth/[...nextauth]
+const handler = nextAuth(authOptions);
+
+// must export by an https request for any route file like this
+export { handler as GET, handler as POST };
